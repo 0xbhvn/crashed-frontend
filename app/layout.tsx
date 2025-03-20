0a5f073365aea61ec9172a5cf1f97b1144e93a6e
Bhaven Kakade <thisisbhaven@gmail.com>
2025-03-21 00:37:59 +0530

    refactor: update RootLayout to include suppressHydrationWarning and simplify ThemeProvider usage for improved rendering consistency

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,15 +27,14 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
+		<html
+			lang="en"
+			suppressHydrationWarning
+		>
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				<ThemeProvider
-					attribute="class"
-					defaultTheme="dark"
-					enableSystem
-				>
+				<ThemeProvider>
 					{children}
 					<Toaster
 						position="top-right"
